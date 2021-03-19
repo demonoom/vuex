42b463b4d74ecd10fa867e2153797168dd978fdf
noom <noom0130@foxmail.com>
2021-03-19 17:53:04 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>hello world</h1>
+    <Foo></Foo>
+    <Bar></Bar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Foo from "@/components/Foo";
+import Bar from "@/components/Bar";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Foo,
+    Bar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
